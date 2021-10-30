6cabf1c156de19bcca124f28231b94849dbd2f53
Francisco Canas <francisco.m.p.canas@gmail.com>
2021-10-30 12:41:18 +0100

    Graphs is still broken

--- src/graph.h
+++ src/graph.h
@@ -1,7 +1,7 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <utility>
+#include <vector>
 #include "arc.h"
 
 using namespace std;
@@ -14,13 +14,33 @@ class Graph
     private:
 
         // Declare the sets of nodes and arcs
-        unordered_map<unsigned int,Node *> Nodes;
-        unordered_map<pair<>, Arc *> Arcs;
+        unordered_map<unsigned int, Node *> Nodes;
+        unordered_map<vector<unsigned int>, Arc *> Arcs;
 
-    // Create ppublic attributes
+    // Create getters, setters and the constructor
     public:
 
+        // Add a node
+        void addNode(Node * node)
+        {
+            Nodes[node->getID()] = node;
+        }
 
+        // Get a node
+        Node * getNode(unsigned int ID)
+        {
+            return Nodes[ID];
+        }
+
+        // Add an arc
+        void addArc(Arc * arc)
+        {
+            vector<unsigned int> IDs = { arc->getFirstNode()->getID(), arc->getSecondNode()->getID() };
+            Arcs[IDs] = arc;
+        }
+
+        // Get an arc
+        Arc * getArc(unsigned int ID1, unsigned int ID2)
 
 };
 
